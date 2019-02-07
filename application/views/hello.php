a3884271f4283e6651872741dba0f7b46fa8fc92
arga1234 <argawirawan1234@gmail.com>
2019-02-07 15:27:21 +0700

    upload

---
+++ application/views/hello.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Creative - Start Bootstrap Theme</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url()?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli|Staatliches" rel="stylesheet">
+    <!-- Custom fonts for this template -->
+    <link href="<?php echo base_url()?>assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="<?php echo base_url()?>assets/vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url()?>assets/css/creative.min.css" rel="stylesheet">
+    <style>
+    div.sticky {
+      position: -webkit-sticky;
+      position: sticky;
+      top: 150px;
+      background-color: transparent;
+      padding-left: 20px;
+      z-index: 100;
+    }
+
+    .testing {
+      letter-spacing: 1px;
+      font-size: 30px;
+      font-family: 'Staatliches', cursive;
+      color: #00C9BC
+    }
+    .testing_2 {
+      letter-spacing: 1px;
+      font-size: 50px;
+      font-family: 'Staatliches', cursive;
+      color: #FFF
+    }
+    .testing_1 {
+      letter-spacing: 1px;
+      font-size: 30px;
+      font-family: 'Staatliches', cursive;
+      color: #000
+    }
+    .title{
+      font-family: 'Staatliches', cursive;
+    }
+    .deskripsi{
+      font-family: 'Muli', sans-serif;
+    }
+    </style>
+
+  </head>
+
+  <body id="page-top">
+     
+    <!-- Navigation -->
+
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <img style="width:30px;margin-right:10px" src="<?php echo base_url()?>assets/img/logo.png" alt="">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">Trip & Business</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#home">HOME</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#about">ABOUT</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#tour">TOUR PACKAGE</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#service">SERVICE</a>
+              </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact">CONTACT</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    
+
+    <header id="home" class="masthead text-center text-white d-flex">
+      <div class="container my-auto">
+        <div class="row">
+          <div class="col-lg-10 mx-auto">
+            <h1 class="text-uppercase">
+              <strong>TRIP AND BUSINESS</strong>
+            </h1>
+            <hr>
+          </div>
+          <div class="col-lg-8 mx-auto">
+            <p class="text-faded mb-5">Manage Your Needs</p>
+            <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a>
+          </div>
+        </div>
+      </div>
+    </header>
+
+
+   <div>
+      <div style="margin-bottom:-50px" class="sticky"><a href="https://wa.me/6285717917558">
+        <img style="width:50px;margin-bottom:-300px" src="<?php echo base_url()?>assets/img/whatsapp.png" alt=""></a>
+        </div>
+      <section id="about" class="bg-primary" >
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-8 mx-auto text-center">
+                <h2 class="section-heading text-white">HELLO</h2>
+                <hr class="light my-4">
+                <p style="line-height:2;letter-spacing: 1px" class="deskripsi text-faded mb-4">Kami adalah perusahaan di bidang manajemen perjalanan dan  event organizer 
+                (MICE) dengan konsep khusus untuk para pelaku bisnis Indonesia. Kami menawarkan layanan jasa kelas 
+                premium untuk penyelenggaraan berbagai kegiatan bisnis dengan perusahaan-perusahaan di ruang lingkup 
+                negara-negara dunia.</p>
+                <a class="btn btn-light btn-xl js-scroll-trigger" href="#craft">Get Started!</a>
+              </div>
+            </div>
+          </div>
+        </section>
+  
+  
+  
+      <header class="masthead_2 text-center text-white d-flex">
+          <div class="container my-auto">
+            <div class=" row">
+              <div class=" col-lg-10 mx-auto">
+                <h1 class="text-uppercase">
+                  <strong>EXPERIENCE TRAVEL</strong>
+                </h1>
+                <hr>
+              </div>
+            </div>
+          </div>
+        </header>
+  
+        <section style="background-color:#212529!important" class="bg-primary" id="craft">
+            <div class="container">
+              <div class="row">
+                <div style="margin-top: -60px;margin-bottom: -100px;" class="col-lg-8 mx-auto text-center">
+                  <h2 class="section-heading text-white">Craft Your Memorable Journey With Us</h2>
+                  <hr class="light my-4">
+                  <p style="line-height:2;letter-spacing: 1px" class="deskripsi text-faded mb-4">Our travel specialists is a mix of professionals with years of on-the-field experiences and young energetic talents with creativity. Our commitment is to continuously deliver high quality services to our customers.
+                      Our travel solutions are and not limited to flight, hotel, transportation, and tours reservation, to meetings, incentive trips, conference or convention, and events or exhibition services.
+                      </p>
+                </div>
+              </div>
+            </div>
+          </section>
+  
+      <header class="masthead_3 text-center text-white d-flex" id="tour">
+          <div class="container my-auto">
+  
+            <div class=" row">
+  
+            <div class=" col-md-12 mx-auto">
+            <p style="text-align:center;font-size: 50px;letter-spacing: 2px" class="testing_2"><b>TOUR PACKAGE</b></p>
+            </div>
+  
+            <div class=" col-md-6 mx-auto">
+             <p style="text-align:center;font-size: 30px;letter-spacing: 2px" class="testing"><b>FIELD TRIP</b></p>
+             <hr style="margin-top:-5px;margin-bottom:10px">
+             <p class="deskripsi" style="text-align:justify;letter-spacing:1px;font-size: 15px;padding-left: 50px;padding-right:50px">Memberikan pembelajaran di luar dari aktivitas biasanya yang dilakukan dengan tujuan untuk mendapatkan pengalaman yang berharga. Kegiatan yang dapat dilakukan, antara lain : mengunjungi objek wisata, meninjau pabrik mobil, peternakan, perkebunan, dan lain sebagainya. Field trip dianggap menjadi metode efektif yang digunakan dalam pembelajaran dalam melatih keterampilan sesuai bidangnya.</p>
+              
+            </div>
+            <div class=" col-md-6 mx-auto">
+            <p style="text-align:center;font-size: 30px;letter-spacing: 2px" class="testing"><b>SHORT COURSE</b></p>
+            <hr style="margin-top:-5px;margin-bottom:10px">
+            <p class="deskripsi" style="text-align:justify;letter-spacing:1px;font-size: 15px;padding-left: 50px;padding-right:50px">Menawarkan program short course yang diperuntukan untuk para profesional yang berkeinginan untuk memperbarui ilmu yang dimiliki sehingga dapat memberikan kontribusi maksimal terhadap institusi atau organisasi tempat bekerja. Short Course meliputi kursus-kursus spesialis bertaraf Internasional dengan waktu penyelenggaraan 2-4 minggu dan diakhiri dengan pemberian sertifikat atau diploma dari institusi terkait.
+            </p>
+            
+            </div>
+            <div class=" col-md-6 mx-auto">
+            <p style="text-align:center;font-size: 30px;letter-spacing: 2px" class="testing"><b>EXCURSION</b></p>
+            <hr style="margin-top:-5px;margin-bottom:10px">
+            <p class="deskripsi" style="text-align:justify;letter-spacing:1px;font-size: 15px;padding-left: 50px;padding-right:50px">Menawarkan program excursion yang mana dapat menjadi pilihan untuk mengunjungi suatu negara atau objek wisata tertentu dalam waktu yang kurang dari 24 jam dengan tetap akan mendapatkan pengalaman berharga dari atraksi wisata yang dikunjungi.</p>
+          
+            </div>
+            <div class=" col-md-6 mx-auto">
+            <p style="text-align:center;font-size: 30px;letter-spacing: 2px" class="testing"><b>SPECIALIZED TOURS</b></p>
+            <hr style="margin-top:-5px;margin-bottom:10px">
+            <p class="deskripsi" style="text-align:justify;letter-spacing:1px;font-size: 15px;padding-left: 50px;padding-right:50px">Menawarkan program excursion yang mana dapat menjadi pilihan untuk mengunjungi suatu negara atau objek wisata tertentu dalam waktu yang kurang dari 24 jam dengan tetap akan mendapatkan pengalaman berharga dari atraksi wisata yang dikunjungi.</p>
+            </div>
+            </div>
+          </div>
+        </header>
+  
+    <section class="bg-primary" id="service">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 mx-auto text-center"><b><p class="title" style="color:#fff;font-size:50px;letter-spacing:2px;margin-top: -50px;margin-bottom: 50px">OUR SERVICE</p></b></div>
+            <div style="margin-bottom:50px" class=" col-md-4 mx-auto">
+            <div class="col-md-12" style="text-align:center;margin-bottom: 20px"><img style="width:50%;margin: auto" src="<?php echo base_url()?>assets/img/concept.png" alt=""></div>
+            <p style="text-align:center;font-size: 30px;letter-spacing: 2px" class="testing_1"><b>CONCEPT & CONSULTACY</b></p>
+            <hr style="margin-top:-5px;margin-bottom:10px">
+            <p class="deskripsi" style="text-align:center;letter-spacing:1px;color:#fff;font-size: 15px;padding-left: 50px;padding-right:50px">Membantu Pembuatan konsep perjalanan sesuai keinginan, antara lain: Event concept, PR & communication strategy, Activation concept.</p>
+            </div>
+            <div  style="margin-bottom:50px" class=" col-md-4 mx-auto">
+            <div class="col-md-12" style="text-align:center;margin-bottom: 20px"><img style="width:50%;margin: auto" src="<?php echo base_url()?>assets/img/hotel.png" alt=""></div>
+            <p style="text-align:center;font-size: 30px;letter-spacing: 2px" class="testing_1"><b>HOTEL ACCOMMODATION & TRANSPORTATION</b></p>
+            <hr style="margin-top:-5px;margin-bottom:10px">
+            <p class="deskripsi" style="text-align:center;letter-spacing:1px;color:#fff;font-size: 15px;padding-left: 50px;padding-right:50px">Menjamin fasilitas penginapan dan transportasi selama berada di destinasi negara tujuan pelanggan.</p>
+            </div>
+            <div style="margin-bottom:50px" class=" col-md-4 mx-auto">
+            <div class="col-md-12" style="text-align:center;margin-bottom: 20px"><img style="width:50%;margin: auto" src="<?php echo base_url()?>assets/img/guide.png" alt=""></div>
+            <p style="text-align:center;font-size: 30px;letter-spacing: 2px" class="testing_1"><b>GUIDING & TRANSLATOR</b></p>
+            <hr style="margin-top:-5px;margin-bottom:10px">
+            <p class="deskripsi" style="text-align:center;letter-spacing:1px;color:#fff;font-size: 15px;padding-left: 50px;padding-right:50px">Menyediakan jasa pemandu dan penerjemah selama dalam perjalanan sehingga dapat menjamin kelancaran komunikasi.</p>
+            </div>
+            <div style="margin-bottom:50px" class=" col-md-4 mx-auto">
+            <div class="col-md-12" style="text-align:center;margin-bottom: 20px"><img style="width:50%;margin: auto" src="<?php echo base_url()?>assets/img/visa.png" alt=""></div>
+            <p style="text-align:center;font-size: 30px;letter-spacing: 2px" class="testing_1"><b>VISA & INSURANCE</b></p>
+            <hr style="margin-top:-5px;margin-bottom:10px">
+            <p class="deskripsi" style="text-align:center;letter-spacing:1px;color:#fff;font-size: 15px;padding-left: 50px;padding-right:50px">Melayani konsultasi pembuatan visa dan menjamin bahwa setiap klien yang melakukan perjalanan akan mendapatkan asuransi perjalanan wisata.</p>
+            </div>
+            <div style="margin-bottom:50px" class=" col-md-4 mx-auto">
+            <div class="col-md-12" style="text-align:center;margin-bottom: 20px"><img style="width:50%;margin: auto" src="<?php echo base_url()?>assets/img/ticketting.png" alt=""></div>
+            <p style="text-align:center;font-size: 30px;letter-spacing: 2px" class="testing_1"><b>TICKETING SERVICE</b></p>
+            <hr style="margin-top:-5px;margin-bottom:10px">
+            <p class="deskripsi" style="text-align:center;letter-spacing:1px;color:#fff;font-size: 15px;padding-left: 50px;padding-right:50px">Menyediakan pelayanan pemesanan segala kebutuhan tiket transportasi perjalanan (darat, laut, udara) ke seluruh destinasi negara tujuan dan tiket acara hiburan seperti theater & olahraga bagi pelanggan.</p>
+            </div>
+            <div  style="margin-bottom:50px" class=" col-md-4 mx-auto">
+            <div class="col-md-12" style="text-align:center;margin-bottom: 20px"><img style="width:50%;margin: auto" src="<?php echo base_url()?>assets/img/airport.png" alt=""></div>
+            <p style="text-align:center;font-size: 30px;letter-spacing: 2px" class="testing_1"><b>AIRPORT ASSISTANT</b></p>
+            <hr style="margin-top:-5px;margin-bottom:10px">
+            <p class="deskripsi" style="text-align:center;letter-spacing:1px;color:#fff;font-size: 15px;padding-left: 50px;padding-right:50px">Membantu segala kebutuhan klien selama berada di bandara seperti check-in dan handling baggage sekaligus menyediakan airport transfer.</p>
+            </div>
+           
+          </div>
+        </div>
+      </section>
+
+      <header class="masthead_4 text-center text-white d-flex">
+          <div class="container my-auto">
+            <div class=" row">
+              <div class=" col-lg-10 mx-auto">
+                <h1 class="text-uppercase">
+                  <strong>LET US HELP U TO RELAX</strong>
+                </h1>
+                <hr>
+              </div>
+            </div>
+          </div>
+        </header>
+  
+      <section style="background-color:#212529!important" id="contact" class="bg-primary" id="about">
+          <div class="container">
+            <div class="row">
+              <div  class="col-md-4">
+                <p style="line-height:2;letter-spacing: 1px" class="deskripsi text-faded mb-4"><b>ALamat</b></p>
+                <p style="line-height:2;letter-spacing: 1px" class="deskripsi text-faded mb-4">T&B Tours Ruko Permata Hijau Lantai 2 Jalan Kebayoran Lama No.9A Jakarta Selatan 122240
+                </p>
+                <p style="line-height:2;letter-spacing: 1px" class="deskripsi text-faded mb-4"><b>Telepon :</b> +6285717917558</p>
+                <p style="line-height:2;letter-spacing: 1px" class="deskripsi text-faded mb-4"><b>Whatsapp :</b> +6285717917558</p>
+                <p style="line-height:2;letter-spacing: 1px" class="deskripsi text-faded mb-4"><b>Email :</b> tripbisnisindonesia@gmail.com</p>
+              </div>
+  
+              <div  class="col-md-4">
+                  <p style="line-height:2;letter-spacing: 1px" class="deskripsi text-faded mb-4"><b>Blog</b></p>
+                  <li style="margin-left:10px;line-height:2;letter-spacing: 1px" class="deskripsi text-faded mb-4">Blog Pertama</li>
+                  <li style="margin-left:10px;line-height:2;letter-spacing: 1px" class="deskripsi text-faded mb-4">Blog Kedua</li>
+                  <li style="margin-left:10px;line-height:2;letter-spacing: 1px" class="deskripsi text-faded mb-4">Blog Ketiga</li>
+                  <p style="line-height:2;letter-spacing: 1px" class="deskripsi text-faded mb-4"><b>Social Media</b></p>
+                  <li style="margin-left:10px;line-height:2;letter-spacing: 1px" class="deskripsi text-faded mb-4">Instagram</li>
+                </div>
+  
+                <div class="col-md-4">
+                    <p style="line-height:2;letter-spacing: 1px" class="deskripsi text-faded mb-4"><b>Sitemap</b></p>
+                    
+                    <li style="margin-left:10px;line-height:2;letter-spacing: 1px" class="deskripsi text-faded mb-4">HOME</li>
+                    <li style="margin-left:10px;line-height:2;letter-spacing: 1px" class="deskripsi text-faded mb-4">GALLERY</li>
+                    <li style="margin-left:10px;line-height:2;letter-spacing: 1px" class="deskripsi text-faded mb-4">BLOG</li>
+                    <li style="margin-left:10px;line-height:2;letter-spacing: 1px" class="deskripsi text-faded mb-4">CONTACT</li>
+                  </div>
+            </div>
+          </div>
+        </section>
+  
+   </div>
+    
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="<?php echo base_url()?>assets/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo base_url()?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="<?php echo base_url()?>assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="<?php echo base_url()?>assets/vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="<?php echo base_url()?>assets/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="<?php echo base_url()?>assets/js/creative.min.js"></script>
+
+  </body>
+
+</html>
